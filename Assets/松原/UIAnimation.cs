50385a74e456d2e7096cd8d28dbfa0f292916d36
haruya <haruya1122@i.softbank.jp>
2022-10-17 18:09:30 +0900

    スコアアニメーションの追加

--- Assets/松原/UIAnimation.cs
+++ Assets/松原/UIAnimation.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class ButtonAnimation : MonoBehaviour
+public class UIAnimation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        TotalScore();
     }
 
     // Update is called once per frame
@@ -25,5 +25,11 @@ public class ButtonAnimation : MonoBehaviour
     public void ButtonUp()
     {
         transform.DOScale(1, 0.1f);
+    }
+
+    public void TotalScore()
+    {
+        transform.DOScale(1.5f, 0.1f);
+        transform.DOScale(1, 0.5f);
     }
 }
